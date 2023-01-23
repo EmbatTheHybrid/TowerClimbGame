46ff7e0ea5222a215eb089ac92314ed0c1b6806b
Embat <grimajenna80@gmail.com>
2023-01-23 17:10:46 +0100

    ADdition of files

---
+++ offline.js
@@ -0,0 +1,12 @@
+﻿{
+	"version": 1674487722,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/floorsprite.png",
+		"images/wall.png"
+	]
+}
