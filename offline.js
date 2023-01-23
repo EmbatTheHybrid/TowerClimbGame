8f6622c29fdd5d26c9903d0d96aa9f1a4796a6ed
Embat <grimajenna80@gmail.com>
2023-01-23 17:19:44 +0100

    Changes

--- offline.js
+++ offline.js
@@ -1,12 +1,33 @@
 ﻿{
-	"version": 1674487722,
+	"version": 1674490702,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/background.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
 		"images/floorsprite.png",
-		"images/wall.png"
+		"images/goblin-sheet0.png",
+		"images/goblin-sheet1.png",
+		"images/directionshift-sheet0.png",
+		"images/key-sheet0.png",
+		"images/exitdoor-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/wall.png",
+		"images/boss-sheet0.png",
+		"images/boss-sheet1.png",
+		"media/scoreupdate.ogg",
+		"media/swordslash.ogg",
+		"media/hurt.ogg",
+		"media/the_old_tower_inn.ogg",
+		"media/defeat.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
 	]
 }
